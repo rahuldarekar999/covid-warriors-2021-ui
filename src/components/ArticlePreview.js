cd7e904f4d0f0cb535eef33b5c9b288bdc547f59
Rahul Darekar <datirkirti09@gmail.com>
2021-04-27 11:48:38 +0530

    initial commit

---
+++ src/components/ArticlePreview.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import agent from '../agent';
+import { connect } from 'react-redux';
+import { ARTICLE_FAVORITED, ARTICLE_UNFAVORITED } from '../constants/actionTypes';
+
+const FAVORITED_CLASS = 'btn btn-sm btn-primary';
+const NOT_FAVORITED_CLASS = 'btn btn-sm btn-outline-primary';
+
+const mapDispatchToProps = dispatch => ({
+  favorite: slug => dispatch({
+    type: ARTICLE_FAVORITED,
+    payload: agent.Articles.favorite(slug)
+  }),
+  unfavorite: slug => dispatch({
+    type: ARTICLE_UNFAVORITED,
+    payload: agent.Articles.unfavorite(slug)
+  })
+});
+
+const ArticlePreview = props => {
+  const article = props.article;
+  const favoriteButtonClass = article.favorited ?
+    FAVORITED_CLASS :
+    NOT_FAVORITED_CLASS;
+
+  const handleClick = ev => {
+    ev.preventDefault();
+    if (article.favorited) {
+      props.unfavorite(article.slug);
+    } else {
+      props.favorite(article.slug);
+    }
+  };
+  const getDate = (time) =>{
+    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    d.setUTCSeconds(time);
+    return d.toLocaleDateString("en-IN");
+  }
+  return (
+    <div className="article-preview">
+      <div className="article-meta">
+        <Link to={`/@${article.author.username}`}>
+          <img src={article.author.image} alt={article.author.username} />
+        </Link>
+        
+        <div className="info">
+          <Link className="author" to={article.chatIdMobileNumber}>
+            {article.chatIdMobileNumber}
+          </Link>
+          <span className="date">
+            {getDate(article.time)}
+          </span>
+        </div>
+      </div>
+
+      <Link to={`/article/${article.slug}`} className="preview-link">
+        <h1>{article.body}</h1>
+      </Link>
+    </div>
+  );
+}
+
+export default connect(() => ({}), mapDispatchToProps)(ArticlePreview);
